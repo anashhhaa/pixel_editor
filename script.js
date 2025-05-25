67010eb4c9f7cd0dfcc3a4876d77e627d1d0b243
anashhhaa <133570089+anashhhaa@users.noreply.github.com>
2025-05-25 10:26:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+let workplace = document.querySelector('.workplace')
+
+let current_color = 'rgb(255, 0, 0)'
+var is_clicked = false
+
+document.addEventListener('mousedown', function(){
+    is_clicked = true
+})
+
+document.addEventListener('mouseup', function(){
+    is_clicked = false
+})
+
+for (let i = 0; i < 800; i += 1){
+    let block = document.createElement('div')
+    block.classList.add('block')
+    workplace.appendChild(block)
+}
+
+let fill = document.querySelector('.fill')
+let blocks = document.querySelectorAll('.block')
+blocks.forEach(block => {
+    block.addEventListener("mouseover", function() {
+        if (is_clicked){
+            anime({
+                targets: block,
+                backgroundColor: current_color,
+                duration: 200,
+                easing: 'linear',
+            })
+        }
+    })
+    fill.addEventListener('click', function(){
+        block.style.backgroundColor = current_color
+    })
+})
+
+blocks.forEach(block => {
+    block.addEventListener("click", function() {
+        anime({
+                targets: block,
+                backgroundColor: current_color,
+                duration: 200,
+                easing: 'linear',
+            })
+    }
+)
+})
+
+let colorBlocks = document.querySelectorAll('.colorBlock')
+colorBlocks.forEach(colorBlock => {
+    colorBlock.addEventListener('click', function() {
+        current_color = getComputedStyle(colorBlock).backgroundColor
+        document.querySelector('.selected').classList.remove('selected')
+        colorBlock.classList.add('selected')
+    })
+})
+
+let earaser = document.querySelector('.earaser')
+earaser.addEventListener('click', function(){
+    current_color = getComputedStyle(workplace).backgroundColor
+    document.querySelector('.selected').classList.remove('selected')
+    earaser.classList.add('selected')
+})
+
+
+
